6dece5a4ba6c3abd54ad0fff7069312f9e1af47d
matiasroldan6 <matias.roldan@southworks.com>
2019-07-17 14:12:40 -0300

    Add method signatures for .NET Twilio adapter. Also, project and folders

---
+++ Microsoft.Bot.Builder.Adapters.Twilio/ITwilioAdapterOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Bot.Builder.Adapters.Twilio
+{
+    public interface ITwilioAdapterOptions
+    {
+        string TwilioNumber { get; set; }
+
+        string AccountSID { get; set; }
+
+        string AuthToken { get; set; }
+
+        string ValidationURL { get; set; }
+    }
+}
---
+++ Microsoft.Bot.Builder.Adapters.Twilio/TwilioAdapter.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Bot.Schema;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace Microsoft.Bot.Builder.Adapters.Twilio
+{
+    public class TwilioAdapter : BotAdapter
+    {
+        public const string NAME = "Twilio SMS Adapter";
+
+        private readonly ITwilioAdapterOptions options;
+
+        public TwilioAdapter(ITwilioAdapterOptions options)
+        {
+        }
+
+        public TwilioBotWorker BotkitWorker { get; private set; }
+
+        /// <summary>
+        /// Standard BotBuilder adapter method to send a message from the bot to the messaging API.
+        /// </summary>
+        /// <param name="turnContext">A TurnContext representing the current incoming message and environment.</param>
+        /// <param name="activities">An array of outgoing activities to be sent back to the messaging API.</param>
+        /// <param name="cancellationToken">A cancellation token for the task.</param>
+        /// <returns>A resource response.</returns>
+        public override async Task<ResourceResponse[]> SendActivitiesAsync(ITurnContext turnContext, Activity[] activities, CancellationToken cancellationToken)
+        {
+            return await Task.FromException<ResourceResponse[]>(new NotImplementedException());
+        }
+
+        /// <summary>
+        /// Accept an incoming webhook request and convert it into a TurnContext which can be processed by the bot's logic.
+        /// </summary>
+        /// <param name="request">A request object from Restify or Express.</param>
+        /// <param name="response">A response object from Restify or Express.</param>
+        /// <param name="bot">A bot with logic function in the form `async(context) => { ... }`.</param>
+        /// <param name="cancellationToken">A cancellation token for the task.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public async Task ProcessAsync(HttpRequest request, HttpResponse response, IBot bot, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            await Task.FromException(new NotImplementedException());
+        }
+
+        /// <summary>
+        /// Standard BotBuilder adapter method to update a previous message with new content.
+        /// </summary>
+        /// <param name="turnContext">A TurnContext representing the current incoming message and environment.</param>
+        /// <param name="activity">The updated activity in the form '{id: `id of activity to update`, ...}'.</param>
+        /// <param name="cancellationToken">A cancellation token for the task.</param>
+        /// <returns>A resource response with the Id of the updated activity.</returns>
+        public override async Task<ResourceResponse> UpdateActivityAsync(ITurnContext turnContext, Activity activity, CancellationToken cancellationToken)
+        {
+            // Twilio adapter does not support updateActivity.
+            return await Task.FromException<ResourceResponse>(new NotImplementedException("Twilio SMS does not support updating activities."));
+        }
+
+        /// <summary>
+        /// Standard BotBuilder adapter method to delete a previous message.
+        /// </summary>
+        /// <param name="turnContext">A TurnContext representing the current incoming message and environment.</param>
+        /// <param name="reference">An object in the form "{activityId: `id of message to delete`, conversation: { id: `id of channel`}}".</param>
+        /// <param name="cancellationToken">A cancellation token for the task.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public override async Task DeleteActivityAsync(ITurnContext turnContext, ConversationReference reference, CancellationToken cancellationToken)
+        {
+            // Twilio adapter does not support deleteActivity.
+            await Task.FromException<ResourceResponse>(new NotImplementedException("Twilio SMS does not support deleting activities."));
+        }
+
+        /// <summary>
+        /// Formats a BotBuilder activity into an outgoing Twilio SMS message.
+        /// </summary>
+        /// <param name="activity">A BotBuilder Activity object</param>
+        /// <returns>a Twilio message object with {body, from, to, mediaUrl}.</returns>
+        private MessageResource ActivityToTwilio(Activity activity)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Standard BotBuilder adapter method for continuing an existing conversation based on a conversation reference.
+        /// </summary>
+        /// <param name="reference">A conversation reference to be applied to future messages.</param>
+        /// <param name="logic">A bot logic function that will perform continuing action in the form 'async(context) => { ... }'.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        private async Task ContinueConversationAsync(ConversationReference reference, BotCallbackHandler logic)
+        {
+            await Task.FromException(new NotImplementedException());
+        }
+
+        /// <summary>
+        /// Verify the signature of an incoming webhook request as originating from Twilio.
+        /// </summary>
+        /// <returns>If signature is valid, returns true. Otherwise, sends a 401 error status via http response and then returns false.</returns>
+        private bool VerifySignature(HttpRequest request, HttpResponse response)
+        {
+            return false;
+        }
+    }
+}

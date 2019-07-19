b0d83a72f1a0d76702cebb92968edecd6cf5dfc6
matiasroldan6 <matias.roldan@southworks.com>
2019-07-19 09:43:37 -0300

    Add copyright notice

--- libraries/Adapters/Microsoft.Bot.Builder.Adapters.Twilio/ITwilioAdapterOptions.cs
+++ libraries/Adapters/Microsoft.Bot.Builder.Adapters.Twilio/ITwilioAdapterOptions.cs
@@ -1,4 +1,7 @@
-﻿namespace Microsoft.Bot.Builder.Adapters.Twilio
+﻿// Copyright(c) Microsoft Corporation.All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Bot.Builder.Adapters.Twilio
 {
     public interface ITwilioAdapterOptions
     {
